7486c36f1762266eb7a547566815cd0f27c7aa0b
JYPark09 <jyp10987@gmail.com>
2021-09-04 18:14:51 +0900

    헬퍼 스크립트 개선

--- C++/test.sh
+++ C++/test.sh
@@ -6,10 +6,12 @@ executable=${target}.out
 
 g++ --std=c++1z -o ${executable} ${src}
 
-for i in `ls input*`
+for i in `ls inputs/*`
 do
 echo "TEST CASE \"${i}\""
 ./${target}.out < ${i}
+echo
+echo
 done
 
 rm ${executable}
